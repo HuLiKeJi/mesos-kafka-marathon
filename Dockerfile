359401b52c79fbac42e9d8e1f803c412b240abb8
Simon Liang <simon@divby0.io>
2016-10-13 20:13:42 +0800

    should be good to go

--- Dockerfile
+++ Dockerfile
@@ -13,10 +13,12 @@ RUN /tmp/download-kafka.sh && tar xfz /tmp/kafka_${SCALA_VERSION}-${KAFKA_VERSIO
 # setup IO volume so we connect with marathon's local persistent storage
 VOLUME ["/kafka"]
 
+ENV KAFKA_HOME /opt/kafka_${SCALA_VERSION}-${KAFKA_VERSION}
+
 # adding setup and run scripts
 ADD src/002-kafka-setup.sh /usr/bin/kafka-setup.sh
 ADD src/003-create-topics.sh /usr/bin/create-topics.sh
-ADD src/004-start-kafka.sh /tmp/start-kafka.sh
-ADD src/999-test.sh /tmp/test.sh
+ADD src/004-start-kafka.sh /usr/bin/start-kafka.sh
 
-CMD ["/tmp/test.sh"]
+# Use "exec" form so that it runs as PID 1 (useful for graceful shutdown)
+CMD ["start-kafka.sh"]
---
+++ src/002-kafka-setup.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+###########################################################################
+
+## these environment vars shouldn't be overridden unless you absolutely have to, since it connects with marathon config
+
+if [[ -z "$KAFKA_PORT" ]]; then
+    # make sure in Marathon json, have one port configured named "kafka"
+    export KAFKA_PORT=$PORT_KAFKA
+fi
+if [[ -z "$KAFKA_LISTENERS" ]]; then
+    # set up listeners
+    export KAFKA_LISTENERS="PLAINTEXT://:$PORT_KAFKA"
+fi
+if [[ -z "$KAFKA_ADVERTISED_HOST_NAME" ]]; then
+    # bind to the hostname Marathon provides
+    export KAFKA_ADVERTISED_HOST_NAME=$HOST
+fi
+if [[ -z "$KAFKA_BROKER_ID" ]]; then
+    # By default auto allocate broker ID
+    export KAFKA_BROKER_ID=-1
+fi
+if [[ -z "$KAFKA_LOG_DIRS" ]]; then
+    # write logs to mesos-sandbox location for easy access
+    export KAFKA_LOG_DIRS="$MESOS_SANDBOX/kafka-logs"
+fi
+
+############################################################################
+
+if [[ -z "$KAFKA_ZOOKEEPER_CONNECT" ]]; then
+    export KAFKA_ZOOKEEPER_CONNECT=$(env | grep ZK.*PORT_2181_TCP= | sed -e 's|.*tcp://||' | paste -sd ,)
+fi
+
+if [[ -n "$KAFKA_HEAP_OPTS" ]]; then
+    sed -r -i "s/(export KAFKA_HEAP_OPTS)=\"(.*)\"/\1=\"$KAFKA_HEAP_OPTS\"/g" $KAFKA_HOME/bin/kafka-server-start.sh
+    unset KAFKA_HEAP_OPTS
+fi
+
+for VAR in `env`
+do
+  if [[ $VAR =~ ^KAFKA_ && ! $VAR =~ ^KAFKA_HOME ]]; then
+    kafka_name=`echo "$VAR" | sed -r "s/KAFKA_(.*)=.*/\1/g" | tr '[:upper:]' '[:lower:]' | tr _ .`
+    env_var=`echo "$VAR" | sed -r "s/(.*)=.*/\1/g"`
+    if egrep -q "(^|^#)$kafka_name=" $KAFKA_HOME/config/server.properties; then
+        sed -r -i "s@(^|^#)($kafka_name)=(.*)@\2=${!env_var}@g" $KAFKA_HOME/config/server.properties #note that no config values may contain an '@' char
+    else
+        echo "$kafka_name=${!env_var}" >> $KAFKA_HOME/config/server.properties
+    fi
+  fi
+done
+
+if [[ -n "$CUSTOM_INIT_SCRIPT" ]] ; then
+  eval $CUSTOM_INIT_SCRIPT
+fi
