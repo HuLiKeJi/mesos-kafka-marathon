4ae5fb6d7761c88f1652b0a4c5ee24256d842331
Simon Liang <simon@divby0.io>
2016-10-15 12:39:29 +0800

    fixed a typo in commands

--- src/003-get-broker-id.sh
+++ src/003-get-broker-id.sh
@@ -5,7 +5,7 @@ BROKER_ID=$(cat /kafka/meta.properties | grep -o "broker\.id=.*" | sed -r "s/bro
 
 if [[ -z "$BROKER_ID" ]]; then
     # if it doesn't exist, grab a new ID from zookeeper using atomic increment
-    export BROKER_ID=$(zookeepercli --server $KAFKA_ZOOKEEPER_CONNECT -c inc /kafka-broker-atom)
+    export BROKER_ID=$(zookeepercli -servers $KAFKA_ZOOKEEPER_CONNECT -c inc /kafka-broker-atom)
 fi
 
 echo $BROKER_ID
